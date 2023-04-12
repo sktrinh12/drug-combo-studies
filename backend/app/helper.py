42b4f831b21467268b711a4e81e40f9af659ea71
spencer.trinh <spencer.trinh@kinnate.com>
2023-04-12 16:35:26 -0700

    add drug1/2 names into data df, autoformat

--- backend/app/helper.py
+++ backend/app/helper.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 from .musyc.combinations.musyc import MuSyC
 from .musyc.utils import plots, dose_tools, np
 import pandas as pd
+
 # from musyc.combinations.musyc import MuSyC
 # from musyc.utils import plots, dose_tools
 
@@ -47,6 +48,7 @@ import pandas as pd
 #         data = plots.generate_3dsur_data(d1, d2, E=E, scatter_points=df)
 #     return data
 
+
 def map_conc_order(conc, unq_conc, reverse=False):
     num_sf = 6
     rev_flag = None
@@ -60,50 +62,60 @@ def map_conc_order(conc, unq_conc, reverse=False):
     if conc == 0.0:
         return 0
     specific_order = np.round_(np.sort(unq_conc)[::rev_flag][:up_to], num_sf)
-    order_dct = {v: k+cnt for k, v in enumerate(specific_order)}
+    order_dct = {v: k + cnt for k, v in enumerate(specific_order)}
     res = order_dct[conc]
     return res
 
-def generate_model_data(df,
-                        e0_bounds,
-                        e1_bounds,
-                        e2_bounds,
-                        e3_bounds,
-                        knte_dtype):
-    print('model running...')
+
+def generate_model_data(df, e0_bounds, e1_bounds, e2_bounds, e3_bounds, knte_dtype):
+    print("model running...")
 
     if knte_dtype:
-        unq_conc1 = np.sort(df['drug1.conc'].unique())
-        unq_conc2 = np.sort(df['drug2.conc'].unique())
-        group_order2 = [[i]*unq_conc2.shape[0] for i in range(int(df.shape[0]/unq_conc2.shape[0]))]
+        unq_conc1 = np.sort(df["drug1.conc"].unique())
+        unq_conc2 = np.sort(df["drug2.conc"].unique())
+        group_order2 = [
+            [i] * unq_conc2.shape[0]
+            for i in range(int(df.shape[0] / unq_conc2.shape[0]))
+        ]
         group_order2 = [item for sublist in group_order2 for item in sublist]
-        df['drug2.index.order'] = df["drug2.conc"].apply(lambda x: map_conc_order(x, unq_conc2))
-        df['drug2.index.group.order'] = group_order2
-        df['drug1.index.order'] = df["drug1.conc"].apply(lambda x: map_conc_order(x, unq_conc1))
-        df = df.sort_values(['drug1.index.order', 'drug2.index.order'])
+        df["drug2.index.order"] = df["drug2.conc"].apply(
+            lambda x: map_conc_order(x, unq_conc2)
+        )
+        df["drug2.index.group.order"] = group_order2
+        df["drug1.index.order"] = df["drug1.conc"].apply(
+            lambda x: map_conc_order(x, unq_conc1)
+        )
+        df = df.sort_values(["drug1.index.order", "drug2.index.order"])
 
-    d1 = df['drug1.conc']
-    d2 = df['drug2.conc']
-    eff = df['effect']
+    d1 = df["drug1.conc"]
+    d2 = df["drug2.conc"]
+    eff = df["effect"]
 
-    model = MuSyC(E0_bounds=e0_bounds,
-                  E1_bounds=e1_bounds,
-                  E2_bounds=e2_bounds,
-                  E3_bounds=e3_bounds)
+    model = MuSyC(
+        E0_bounds=e0_bounds,
+        E1_bounds=e1_bounds,
+        E2_bounds=e2_bounds,
+        E3_bounds=e3_bounds,
+    )
 
     model.fit(d1, d2, eff, bootstrap_iterations=100)
 
     E = model.E(d1, d2)
-    print('generate data from model...')
-    data = plots.generate_3dsur_data(d1, d2, E=E, scatter_points=df, knte_dtype=knte_dtype)
+    print("generate data from model...")
+    data = plots.generate_3dsur_data(
+        d1, d2, E=E, scatter_points=df, knte_dtype=knte_dtype
+    )
     params = model.get_parameters(confidence_interval=95)
     # print(params)
     # print(''.join(['-']*10))
     summary = model.summary(confidence_interval=95)
     # print(summary)
-    data = { **data, **params }
+    data = {**data, **params}
     data["summary"] = summary
+    data["drug1.name"] = df["drug1.name"].iloc[0]
+    data["drug2.name"] = df["drug2.name"].iloc[0]
     return data
+
 
 def exec_sql(drug1, drug2, block_id, conn):
     cur = conn.cursor()
@@ -119,17 +131,20 @@ def exec_sql(drug1, drug2, block_id, conn):
     data = cur.fetchall()
     cur.close()
     data = pd.DataFrame(data, columns=names)
-    data.columns = ['block_id', 'drug1.conc', 'drug2.conc', 'effect']
-    if block_id not in data['block_id']:
+    data.columns = ["block_id", "drug1.conc", "drug2.conc", "effect"]
+    if block_id not in data["block_id"]:
         return pd.DataFrame()
-    data = data[data['block_id'] == block_id]
-    data = data[['drug1.conc', 'drug2.conc', 'effect']]
-    zero_conc = data[(data['drug1.conc'] == 0) & (data['drug2.conc'] == 0)]['effect'].item()
-    data['effect'] = data['effect'].apply(lambda x: round(x/zero_conc, 5))
-    data['effect'] = pd.to_numeric(data['effect'])
-    data['drug1.conc'] = pd.to_numeric(data['drug1.conc'])
-    data['drug2.conc'] = pd.to_numeric(data['drug2.conc'])
+    data = data[data["block_id"] == block_id]
+    data = data[["drug1.conc", "drug2.conc", "effect"]]
+    zero_conc = data[(data["drug1.conc"] == 0) & (data["drug2.conc"] == 0)][
+        "effect"
+    ].item()
+    data["effect"] = data["effect"].apply(lambda x: round(x / zero_conc, 5))
+    data["effect"] = pd.to_numeric(data["effect"])
+    data["drug1.conc"] = pd.to_numeric(data["drug1.conc"])
+    data["drug2.conc"] = pd.to_numeric(data["drug2.conc"])
     return data
+
 
 def drug_list(conn):
     cur = conn.cursor()
@@ -137,10 +152,10 @@ def drug_list(conn):
             SELECT DISTINCT DRUG_{0}
             FROM DRUG_COMBO_DATA
             """
-    cur.execute(query.format('ROW'))
+    cur.execute(query.format("ROW"))
     row_drugs = cur.fetchall()
     row_drugs = [d[0] for d in row_drugs]
-    cur.execute(query.format('COL'))
+    cur.execute(query.format("COL"))
     col_drugs = cur.fetchall()
     col_drugs = [d[0] for d in col_drugs]
     cur.close()
