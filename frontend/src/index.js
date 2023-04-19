2eb49c34e62af056de77fc91d13caa7a48b16abf
spencer.trinh <spencer.trinh@kinnate.com>
2023-04-18 17:08:33 -0700

    rename testing routes, change text for wrong endpoint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,24 +6,24 @@ import MainContent from './components/MainContent'
 import CsvUploader from './components/CsvUploader'
 import Display from './components/Display'
 import ScoreTable from './components/ScoreTable'
+import Test from './components/Test'
 
 const rootElement = document.getElementById('root')
 render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='test-plot' element={<MainContent route={'test'} />} />
-      <Route path='file-plot' element={<MainContent route={'files'} />} />
-      <Route path='cmbdb-plot' element={<MainContent route={'sql'} />} />
+      <Route path='test' element={<MainContent route={'files'} />} />
+      <Route path='test2' element={<Test />} />
+      <Route path='db' element={<MainContent route={'sql'} />} />
       <Route path='visualise' element={<Display />} />
-      <Route path='upload-data' element={<CsvUploader />} />
       <Route path='upload' element={<CsvUploader />} />
       <Route path='score' element={<ScoreTable />} />
       <Route
         path='*'
         element={
           <main style={{ padding: '1rem' }}>
-            <p>There's nothing here!</p>
+            <p>That endpoint does not exist!</p>
           </main>
         }
       />
