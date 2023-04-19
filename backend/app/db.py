57395bbec3ab6593364640c8de96fbe96fb1bbd0
spencer.trinh <spencer.trinh@kinnate.com>
2023-04-18 17:29:29 -0700

    use of env vars for credentials

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,9 @@
 import psycopg2
+from os import getenv
 
-conn = psycopg2.connect(user="postgres",
-                         password="postgres",
-                         host="localhost",
-                         database="postgres")
+conn = psycopg2.connect(
+    user=getenv("POSTGRES_USER", "postgres"),
+    password=getenv("POSTGRES_PASSWORD", "postgres"),
+    host=getenv("POSTGRES_HOST", "localhost"),
+    database=getenv("POSTGRES_DBNAME", "postgres"),
+)
