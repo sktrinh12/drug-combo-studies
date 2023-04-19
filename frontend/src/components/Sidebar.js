ef68b3327f0006a50274b1db66ac229b1715b48c
spencer.trinh <spencer.trinh@kinnate.com>
2023-04-18 17:06:12 -0700

    add table and tbody prevent console error

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,80 +1,84 @@
-import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { MathJax, MathJaxContext } from 'better-react-mathjax'
 
 export default function Sidebar(props) {
   const splitValues = (arr) => {
-    return arr.split("\n").map((e) => e.split("\t"));
-  };
+    return arr.split('\n').map((e) => e.split('\t'))
+  }
   const convertTex = (str) => {
-    let prefixStr = str.match("[a-zA-Z]{1,}");
-    let suffixStr = str.match("[1-9]{1,}");
+    let prefixStr = str.match('[a-zA-Z]{1,}')
+    let suffixStr = str.match('[1-9]{1,}')
     // console.log(prefixStr);
     // console.log(suffixStr);
     switch (prefixStr[0]) {
-      case "beta":
-        return `$$\\beta$$`;
-      case "alpha":
-        return `$$\\alpha_{${suffixStr}}$$`;
-      case "gamma":
-        return `$$\\gamma_{${suffixStr}}$$`;
+      case 'beta':
+        return `$$\\beta$$`
+      case 'alpha':
+        return `$$\\alpha_{${suffixStr}}$$`
+      case 'gamma':
+        return `$$\\gamma_{${suffixStr}}$$`
       default:
-        return str;
+        return str
     }
-  };
+  }
 
   const mathJaxConfig = {
-    loader: { load: ["[tex]/html"] },
+    loader: { load: ['[tex]/html'] },
     tex: {
-      packages: { "[+]": ["html"] },
-      inlineMath: [["$", "$"]],
-      displayMath: [["$$", "$$"]],
+      packages: { '[+]': ['html'] },
+      inlineMath: [['$', '$']],
+      displayMath: [['$$', '$$']],
     },
-  };
+  }
 
   const setTextColour = (str) => {
     try {
-      let findStr = str.match("syn.*");
+      let findStr = str.match('syn.*')
       // console.log(findStr);
-      return findStr.length > 0 ? "blue" : "red";
+      return findStr.length > 0 ? 'blue' : 'red'
     } catch {
-      return "red";
+      return 'red'
     }
-  };
+  }
 
-  const calcValues = splitValues(props.calcValues);
+  const calcValues = splitValues(props.calcValues)
 
-  // console.log(calcValues);
+  console.log(calcValues)
   return (
     <div
-      className={props.expanded ? "sidebar sidebar--expanded" : "sidebar"}
+      className={props.expanded ? 'sidebar sidebar--expanded' : 'sidebar'}
       onClick={props.toggleSidebar}
     >
-      {calcValues.length < 2 ? (
-        <p>{calcValues[0]}</p>
-      ) : (
-        calcValues.map((str, i) => (
-          <tr key={i}>
-            <MathJaxContext config={mathJaxConfig} version={3}>
-              <th>
-                <MathJax dynamic inline>
-                  {convertTex(str[0])}
-                </MathJax>
-              </th>
-              <td>
-                {" "}
-                <p>
-                  ={str[1]} {str[2]}{" "}
-                </p>{" "}
-              </td>
-              <td>
-                {" "}
-                <span style={{ color: setTextColour(str[3]) }}>
-                  {" " + str[3]}
-                </span>
-              </td>
-            </MathJaxContext>
-          </tr>
-        ))
-      )}
+      <table>
+        <tbody>
+          {calcValues.length < 2 ? (
+            <p>{calcValues[0]}</p>
+          ) : (
+            calcValues.map((str, i) => (
+              <tr key={i}>
+                <MathJaxContext config={mathJaxConfig} version={3}>
+                  <th>
+                    <MathJax dynamic inline>
+                      {convertTex(str[0])}
+                    </MathJax>
+                  </th>
+                  <td>
+                    {' '}
+                    <p>
+                      ={str[1]} {str[2]}{' '}
+                    </p>{' '}
+                  </td>
+                  <td>
+                    {' '}
+                    <span style={{ color: setTextColour(str[3]) }}>
+                      {' ' + str[3]}
+                    </span>
+                  </td>
+                </MathJaxContext>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
